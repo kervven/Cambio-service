0445056e5017305a52585507fbbd378ad3091796
Kerven Kildhery <kervensilvva@gmail.com>
2024-01-05 16:54:44 -0300

    a resolver conflitos com integração com banco de dados e flyway

---
+++ src/main/resources/db/migration/V1__Create_Table_Cambio.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE `cambio` (
+                          `id` INT AUTO_INCREMENT PRIMARY KEY,
+                          `from_currency` CHAR(3) NOT NULL,
+                          `to_currency` CHAR(3) NOT NULL,
+                          `conversion_factor` DECIMAL(65,2) NOT NULL
+);
+
